85ebd3b556575fd923c462cd2dfb81394d3b5491
The-Quill <baileysheather.98@gmail.com>
2016-02-28 23:26:06 +1000

    added direct stdin prompting method

--- start.js
+++ start.js
@@ -1,5 +1,33 @@
 const core      = require('./chat_modules/core');
 const prompt    = require('./chat_modules/prompt');
+
+function ask(question, format, callback) {
+    var stdin = process.stdin,
+        stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(question + ": ");
+
+    stdin.once('data', function(data) {
+        data = data.toString().trim();
+
+        if (format.test(data)) {
+            callback(data);
+        } else {
+            //stdout.write("It should match: " + format + "\n");
+            console.log(format);
+            ask(question, format, callback);
+        }
+    });
+}
 core.start().then(function(){
-    prompt.start();
+
+    ask("Name", /.+/, function(name) {
+        ask("Email", /^.+@.+$/, function(email) {
+            console.log("Your name is: ", name);
+            console.log("Your email is:", email);
+
+            process.exit();
+        });
+    });
 });
